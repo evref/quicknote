5a93cee6889fffdd0d139fe055d5f30323b816a5
Emil Hultcrantz <emilhtz@gmail.com>
2023-08-30 21:10:12 +0200

    Fix to variable path

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import os
 
-from note import quick_note
 from menu import main_menu
 
-
 def create_save_path():
-    home = os.path.expanduser("~")
-    path = os.path.join(home, "projects/evref/local-scripts/quicknote/data")
+    path = os.path.join(os.path.dirname(__file__), "data")
 
     if not os.path.exists(path):
-        raise Exception("Save directory does not exist")
+        os.makedirs(path)
 
     path = os.path.join(path, "note_list.txt")
+
+    if not os.path.exists(path):
+        open(path, "x")
     
     return path
 
 save_path = create_save_path()
-quick_note(save_path)
-#main_menu(save_path)
+main_menu(save_path)
