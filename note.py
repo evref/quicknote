b2488db890184b71ab825ecfbfa9d7bec47d1c4c
evref <isak.lefevre@hotmail.se>
2023-09-02 09:07:05 +0200

    add dynamic text cursor coloring

--- note.py
+++ note.py
@@ -12,7 +12,8 @@ def quick_note(path):
     entry = tk.Entry(
         fg=FG_WHITE, 
         bg=BG_GREY, 
-        insertbackground=FG_WHITE,
+        insertbackground=BG_GREY,
+        insertofftime=0,
         font=("Consolas", 40),
         width=40)
     entry.pack()
@@ -37,9 +38,18 @@ def quick_note(path):
         window.destroy()
         menu.main_menu(path)
 
+    def on_key_pressed(event):
+        cur_idx = entry.index('insert')
+        cur_len = len(entry.get())
+        if cur_idx < cur_len:
+            entry["insertbackground"] = FG_WHITE
+        else:
+            entry["insertbackground"] = BG_GREY
+
     entry.bind('<Return>', on_enter_pressed)
     entry.bind('<Escape>', on_escape_pressed)
     entry.bind('q', on_escape_pressed)
     entry.bind('<Control-m>', on_menu_pressed)
+    entry.bind('<KeyRelease>', on_key_pressed)
 
     window.mainloop()
